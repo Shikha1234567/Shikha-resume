178923000ba7a51adea9d847db7f42bd94fd4038
deep shikha <deepshikha17.hit@gmail.com>
2022-07-23 14:54:15 +0530

    New updates in portfolio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Shikha-resume
+# Pravin-Resume
+Basic description resume where we are showcasing every career details, example: Qualifications,certifications, projects and work experieces.
+
+Project Lead
+
+Shikha & Pravin
---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+  $('#downloadCV').click(function(e){
+   e.preventDefault();
+   alert("success");
+   window.location.href="Resume2.pdf"
+  });
+
+  
+
+});
